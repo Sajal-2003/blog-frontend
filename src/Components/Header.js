00d4d39f5a6de6d3166da103d4d215087476bc73
Sajal Aggarwal <sajal72003@gmail.com>
2024-05-26 21:37:59 +0530

    logout commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import { message } from "antd";
 import React, { useContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/userContext";
+import axios from "axios";
 
 const Header = () => {
   const { setUserInfo, userInfo } = useContext(UserContext);
